4197d9b6cc4e979bf93015fdff7ceb312bc0abed
HisashiImahayashi <yoshithune1192@gmail.com>
2018-02-10 19:06:43 +0900

    Travisテスト

--- Test.php
+++ Test.php
@@ -1,7 +1,20 @@
 <?php
-class Test extends \PHPUnit\Framework\TestCase
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
 {
     public function testOnePlusOne() {
         $this->assertEquals(1+1,2);
     }
+    
+    public function testOneMinusOne() {
+        $this->assertEquals(1-1,0);
+    }
+    
+    public function testEmpty() {
+        $stack = [];
+        $this->assertEmpty($stack);
+
+        return $stack;
+    }    
 }
